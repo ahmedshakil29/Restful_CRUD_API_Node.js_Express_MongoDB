13037dba5cc288ac9aeda5d7dd1b6ce8c1dc43f2
shakil ahmed <ahmedshakil.aiub@gmail.com>
2023-06-13 13:03:24 +0600

    post product

--- server.js
+++ server.js
@@ -36,6 +36,16 @@ app.get("/products/:id", async (req, res) => {
   }
 });
 
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // Database connect
 
 mongoose
