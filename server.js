05e59031887c0f321711099b6b08d1bbab4e0833
shakil ahmed <ahmedshakil.aiub@gmail.com>
2023-06-13 13:03:52 +0600

    update product

--- server.js
+++ server.js
@@ -46,6 +46,24 @@ app.post("/products", async (req, res) => {
   }
 });
 
+// update a product
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    // we cannot find any product in database
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // Database connect
 
 mongoose
