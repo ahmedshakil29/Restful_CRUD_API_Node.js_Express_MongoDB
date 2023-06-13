490e1401f4c536663bcbe3ed71649c3fcc9be28e
shakil ahmed <ahmedshakil.aiub@gmail.com>
2023-06-13 12:14:26 +0600

    add mongoose

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.get("/", function (req, res) {
   res.send("Hello World 2 ");
 });
 
-app.listen(3000);
+// app.listen(3000);
+
+mongoose
+  .connect(
+    "mongodb+srv://Admin:CrudAdmin@crudapi.fxgmd4x.mongodb.net/CrudApi?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to MongoDB");
+    app.listen(3000, () => {
+      console.log(`App is running on port 3000`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
